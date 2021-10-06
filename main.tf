437225d594807c672b4d78f3237bb76339ad9ac7
rajesh <ykrajesh@outlook.com>
2021-10-06 14:05:56 +0530

    addming another resource block

---
+++ main.tf
@@ -0,0 +1,12 @@
+data "azurerm_resource_group" "rgname" {
+name = var.rgname
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnetname
+  address_space       = var.vnet_cidr
+  location = data.azurerm_resource_group.rgname.location
+  resource_group_name = data.azurerm_resource_group.rgname.name
+}
+resource "azurerm_storage" "name" {
+  
+}   
